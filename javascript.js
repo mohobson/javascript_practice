61973b17e9f5c567070a848af14226ec875972ad
Mo Hobson <mohobson8@gmail.com>
2023-01-08 15:31:27 -0500

    practice button

--- javascript.js
+++ javascript.js
@@ -7,4 +7,30 @@ let user = 'mo';
 let age = '25';
 let message = 'hello';
 
-alert(message); // hello
+// alert(message); // hello
+
+
+// exercise:
+let admin;
+let name = 'john';
+
+admin = name;
+// alert(admin);
+//
+
+// buttom practice
+const btn = document.querySelector("button");
+const txt = document.querySelector("p");
+
+btn.addEventListener("click", updateBtn);
+
+function updateBtn() {
+  if (btn.textContent === "Start machine") {
+    btn.textContent = "Stop machine";
+    txt.textContent = "The machine has started!";
+  } else {
+    btn.textContent = "Start machine";
+    txt.textContent = "The machine is stopped.";
+  }
+}
+//
